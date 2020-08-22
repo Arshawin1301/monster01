adf2326d37c4c9664ae4747c2b9d93be1de844ce
unknown <chaturvedi.sukant@gmail.com>
2020-08-22 18:36:04 +0530

    Updates

---
+++ precache-manifest.8038a668925a3cccf5831b23aeb0fb4a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bcd83e8cab7191cb3d0feb0fda1a567",
+    "url": "/monster01/index.html"
+  },
+  {
+    "revision": "9a9f32f5d0e57916050a",
+    "url": "/monster01/static/css/main.aad94c83.chunk.css"
+  },
+  {
+    "revision": "9dadd1a5607abc36395e",
+    "url": "/monster01/static/js/2.db3844ca.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster01/static/js/2.db3844ca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9a9f32f5d0e57916050a",
+    "url": "/monster01/static/js/main.4f767d19.chunk.js"
+  },
+  {
+    "revision": "6e9c4b404c36eac76b6b",
+    "url": "/monster01/static/js/runtime-main.629e0f6b.js"
+  }
+]);
